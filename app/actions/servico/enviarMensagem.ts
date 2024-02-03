1b669c05d0471c4f19054a4a4cce1b677a4fca88
edoardo colares <edoardo.colares@defensoria.ro.def.br>
2024-02-02 21:18:52 -0300

    refatorando

--- app/actions/servico/enviarMensagem.ts
+++ app/actions/servico/enviarMensagem.ts
@@ -3,9 +3,9 @@
 import { EventoModelo } from '@/app/agenda/Contexto'
 
 import ApiServer from '@/app/fetch/ApiServer'
-// import ApiWhatsapp from '@/app/fetch/ApiWhatsapp'
+import ApiWhatsapp from '@/app/fetch/ApiWhatsapp'
+import { converterData } from '@/app/utils'
 
-// import { converterData } from '@/app/utils'
 type TipoBody = Record<string, string | number | undefined>
 export default async function enviarMensagem(formDataRaw: FormData) {
   const convertendo = formDataRaw as unknown
@@ -23,41 +23,46 @@ export default async function enviarMensagem(formDataRaw: FormData) {
   const resposta = await apiClient.post('evento/', bodyRequest)
 
   if (resposta['mensagem'] == 'Sucesso') {
-    // const sequenciaTemplate = {
-    //   servico: formData.servico.nome,
-    //   valor: formData.servico.valor,
-    //   tempo: formData.servico.tempo_servico,
-    //   data_inicio: converterData(formData.data_inicio),
-    //   hora: formData.hora,
-    //   profissional: formData.profissional?.nome,
-    // }
-    // const apiWhatsapp = new ApiWhatsapp('239619722559434', formData.numero)
-    // const response = await apiWhatsapp.enviarMensagem(sequenciaTemplate)
-    // const resultado = await response.json()
+    const sequenciaTemplate = {
+      servico: formData.servico.nome,
+      valor: formData.servico.valor,
+      tempo: formData.servico.tempo_servico,
+      data_inicio: converterData(
+        formData.data_inicio ? formData.data_inicio : '',
+      ),
+      hora: formData.hora,
+      profissional: formData.profissional?.nome,
+    }
+    const apiWhatsapp = new ApiWhatsapp(
+      '239619722559434',
+      formData.numero ? formData.numero : '',
+    )
+    const response = await apiWhatsapp.enviarMensagem(sequenciaTemplate)
+    const resultado = await response.json()
     console.log('whatsapp')
-    // console.log(resultado)
-    // if (resultado['error']['message']) {
-    //   const error = resultado['error']['message']
-    //   return {
-    //     sucesso: true,
-    //     messagem: 'Inserido com sucesso no agendamento',
-    //     mensagemEnviadaWhatsapp: false,
-    //     errorWhat: error,
-    //   }
-    // }
-    // const mensagemEnvidada = resultado['messages'][0].message_status
-    // if (mensagemEnvidada == 'accepted') {
-    //   return {
-    //     sucesso: true,
-    //     messagem: 'Inserido com sucesso no agendamento',
-    //     mensagemEnviadaWhatsapp: true,
-    //   }
-    // } else {
-    //   return {
-    //     sucesso: true,
-    //     messagem: 'Inserido com sucesso no agendamento',
-    //     mensagemEnviadaWhatsapp: false,
-    //   }
-    // }
+    console.log(resultado)
+    if (resultado['error']['message']) {
+      const error = resultado['error']['message']
+      return {
+        sucesso: true,
+        messagem: 'Inserido com sucesso no agendamento',
+        mensagemEnviadaWhatsapp: false,
+        errorWhat: error,
+      }
+    }
+    const mensagemEnvidada = resultado['messages'][0].message_status
+    if (mensagemEnvidada == 'accepted') {
+      return {
+        sucesso: true,
+        messagem: 'Inserido com sucesso no agendamento',
+        mensagemEnviadaWhatsapp: true,
+      }
+    } else {
+      return {
+        sucesso: true,
+        messagem: 'Inserido com sucesso no agendamento',
+        mensagemEnviadaWhatsapp: false,
+      }
+    }
   }
 }
